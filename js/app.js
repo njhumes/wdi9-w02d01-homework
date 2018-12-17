63407e83b0100bf968d0562bcc0712e8ce3b6bc3
Nicholas Humes <njhumes@gmail.com>
2018-12-17 16:01:20 -0700

    your name object

--- js/app.js
+++ js/app.js
@@ -11,3 +11,13 @@
 // A list of student names from our class, each with a location and each with a list of favorite tv shows.
 // object
 
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+console.log(rainbow[4]);
+const nick = {
+    favoriteFood: 'ramen',
+    hobby: 'playing guitar',
+    location: 'Boulder',
+    favoriteDataType: 'string'
+}
+console.log(nick.hobby);
+
