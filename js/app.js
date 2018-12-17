519fda42a2b984d329e8d0a9011a09ae4b25103d
Nicholas Humes <njhumes@gmail.com>
2018-12-17 15:57:43 -0700

    First Questions

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// //A light switch that can be either on or off.
+// // Boolean
+// A user's email address.
+// String
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// Object
+// A list of student names from our class.
+// Array
+// A list of student names from our class, each with a location.
+// Object
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// object
+
